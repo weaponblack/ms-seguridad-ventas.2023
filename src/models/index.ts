4b2bf9992acc52f2f9cdd0c5029b1b572c35f132
Juan José Gamboa <juanose5427@gmail.com>
2023-03-13 17:12:22 -0500

    Revertir cambios de la creacion no debida de logica de negocios

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,8 @@
-export * from './usuario.model';
-export * from './login.model';
-export * from './rol.model';
-export * from './menu.model';
-export * from './rol-menu.model';
 export * from './credenciales.model';
 export * from './factor-de-autenticacion-porcodigo.model';
+export * from './login.model';
+export * from './menu.model';
+export * from './rol-menu.model';
+export * from './rol.model';
+export * from './usuario.model';
+
